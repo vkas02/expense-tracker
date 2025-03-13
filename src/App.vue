a14e2ec0c2c68361b6d5ce19ae7404fb2911d3f7
bluevice <wizardblue84@gmail.com>
2025-03-13 15:56:38 +0530

    feat- setup and structuring done

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <Header/>
+  <div class="container">
+    <Balance/>
+    <incomeExpenses/>
+    <TransactionList/>
+    <AddTransaction/>
+  </div>
+
+</template>
+
+
+<script>
+import Header from './components/Header.vue'
+import Balance from "./components/Balance.vue";
+import incomeExpenses from "./components/IncomeExpenses.vue";
+import TransactionList from "./components/TransactionList.vue";
+import AddTransaction from "./components/AddTransaction.vue";
+export default {
+  components: {
+    Header,
+    Balance,
+    incomeExpenses,
+    TransactionList,
+    AddTransaction
+  }
+}
+</script>
